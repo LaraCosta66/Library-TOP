11776fd69f1b38fa965444a27777f01cb8466d7c
Lara Costa <larasmila64@gmail.com>
2023-04-14 17:47:06 -0300

    fix: addBookToLibrary function

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
-
-let myLibrary = [
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+const myLibrary = [
   {
     title: "The Seven Husbands of Evelyn Hugo",
     author: "Taylor Jenkins Reid",
     pages: 354,
-    read: true
+    read: true,
   },
 ];
-function Book(title, author, pages, isRead) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
-  }
 
 //DOM Objects
-const booksGrid = document.querySelector('#booksGrid')
-const btnCard = document.querySelector('#btnCard')
-const form = document.querySelector('#addBookForm')
-const titleInput = document.querySelector('#title')
-const authorInput = document.querySelector('#author')
-const pagesInput = document.querySelector('#pages')
-const isRead = document.querySelector('#isRead')
-
-
+const booksGrid = document.querySelector("#booksGrid");
+const btnCard = document.querySelector("#btnCard");
+const form = document.querySelector("#addBookForm");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const isRead = document.querySelector("#isRead");
 
 function addBookToLibrary(event) {
-  event.preventDefault(); 
+  event.preventDefault();
   const titleValue = titleInput.value;
-   const authorValue = authorInput.value;
-    const pagesValue = pagesInput.value;
-    
+  const authorValue = authorInput.value;
+  const pagesValue = pagesInput.value;
+
+    const newBook = new Book(titleValue, authorValue, pagesValue);
+    myLibrary.push(newBook);
+    if(newBook != '') {
+      const newDiv = document.createElement('div');
+      newDiv.innerText = `Titulo: ${newBook.title}, Author: ${newBook.author}, Number of Pages: ${newBook.pages}`;
+      booksGrid.appendChild(newDiv);
+      return newBook;
+    }else{
+      alert('adicione valores')
+    }
   
-      // const title = document.createElement('span')
-      // const author = document.createElement('span')
-      // const pages = document.createElement('span')
-   
-      // library.textContent = my
-      // author.textContent = authorValue
-      // pages.textContent = pagesValue
+}
+// function addBookToLibrary(event) {
+//   event.preventDefault();
+//   const titleValue = titleInput.value;
+//   const authorValue = authorInput.value;
+//   const pagesValue = pagesInput.value;
+//   const newBook = new Book(titleValue, authorValue, pagesValue);
+//   myLibrary.push(newBook);
  
-      // booksGrid.appendChild(title)
-      // booksGrid.appendChild(author)
-  
+// }
 
-      const newBook = new Book(titleValue, authorValue, pagesValue);
-      myLibrary.push(newBook);
-     return console.log(myLibrary)
-  }
+// const title = document.createElement('span')
+// const author = document.createElement('span')
+// const pages = document.createElement('span')
 
+// library.textContent = my
+// author.textContent = authorValue
+// pages.textContent = pagesValue
 
+// booksGrid.appendChild(title)
+// booksGrid.appendChild(author)
 
-  btnCard.addEventListener('click', addBookToLibrary);
+btnCard.addEventListener("click", addBookToLibrary);
